f1700bd63997bee5681f7d2ce5bd06c4745f444f
Marius Mandal <mariusmandal@gmail.com>
2013-10-16 11:12:23 +0200

    json headers

--- ajax_compress.inc.php
+++ ajax_compress.inc.php
@@ -1,4 +1,8 @@
 <?php
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
 $running = new SQL("SELECT `id`
 					FROM `ukm_bilder`
 					WHERE `pl_id` = '#pl_id'
