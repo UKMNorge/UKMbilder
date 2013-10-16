bd7443c804085dc049e812bf73a6f0972557db00
Marius Mandal <mariusmandal@gmail.com>
2013-10-16 16:57:54 +0200

    production

--- ajax_image_move.inc.php
+++ ajax_image_move.inc.php
@@ -19,26 +19,22 @@ foreach($_POST['images'] as $post_id) {
 	global $wpdb;
 	require_once(ABSPATH . 'wp-admin/includes/image.php');
 	require_once(ABSPATH . 'wp-includes/post.php');
-/*
 	$wpdb->update($wpdb->posts,
 				  array('post_title' => $innslag->get('b_name')),
 			      array('ID' => $post_id)
 				 );
-*/
 
 	global $blog_id;
 	// UPDATE UKM_BILDER
 	$sql = new SQLins('ukm_bilder', array('wp_post' => $post_id, 'wp_blog' => $blog_id));
 	$sql->add('b_id', $innslag->g('b_id'));
-	//$sql->run();
-	echo $sql->debug();
+	$sql->run();
 
 	// UPDATE WP_RELATED
 	$sql = new SQLins('ukmno_wp_related', array('post_id' => $post_id,
 												'blog_id' => $blog_id,
 												'post_type' => 'image'));
 	$sql->add('b_id', $innslag->g('b_id'));
-//	$sql->run();
-	echo $sql->debug();
+	$sql->run();
 }
 die(json_encode(array('success' => true, 'b_id' => $_POST['b_id'])));
