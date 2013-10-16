14e883657505e14d47461c7af20d4db43ddfdb8d
Marius Mandal <mariusmandal@gmail.com>
2013-10-16 12:44:16 +0200

    alle inslag preloaded

--- controller_upload.inc.php
+++ controller_upload.inc.php
@@ -1,4 +1,6 @@
 <?php
 $program = $monstring->forestillinger();
+$alle_innslag = $monstring->innslag();
 
-$INFOS = array('program' => $program);
+
+$INFOS = array('program' => $program, 'alle_innslag' => $alle_innslag);
