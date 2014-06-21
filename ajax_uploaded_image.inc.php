00e4c7461c6a29ab8f1664b84ea48cf6d70ccdaa
Daniel Skogstad <daniel.skogstad@gmail.com>
2014-06-22 01:20:27 +0200

    viser filnavn på opplastede filer + bedre håndteringa av feilet opplasting

--- ajax_uploaded_image.inc.php
+++ ajax_uploaded_image.inc.php
@@ -25,30 +25,34 @@ $extension = pathinfo($filename, PATHINFO_EXTENSION);
 $name = $season.'_'.$place.'_'.$id.'.'.$extension;
 $path = $SYNC_FOLDER.$name;
 
-$sql = new SQLins('ukm_bilder', array('id' => $id));
-$sql->add('filename', $name);
-$sql->run();
+if(move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
 
-move_uploaded_file($_FILES['image']['tmp_name'], $path);
+    $sql = new SQLins('ukm_bilder', array('id' => $id));
+    $sql->add('filename', $name);
+    $sql->run();
 
-// RESIZE IMAGE
-$image = new Imagick( $path );
-$imageprops = $image->getImageGeometry();
+    // RESIZE IMAGE
+    $image = new Imagick( $path );
+    $imageprops = $image->getImageGeometry();
+    
+    	// Find proportions
+    	$width = $height = 3800;
+    	if($imageprops['width'] > $imageprops['height']) {
+    		$height = 0;
+    		$compare = 'width';
+    	} else {
+    		$width = 0;
+    		$compare = 'height';
+    	}
+    
+    // IF IMAGE IS LARGER THAN TARGET, RESIZE
+    if($imageprops[$compare] > $$compare) {
+    	$image->scaleImage($width, $height);
+    	$image->writeImage($path);
+    }
 
-	// Find proportions
-	$width = $height = 3800;
-	if($imageprops['width'] > $imageprops['height']) {
-		$height = 0;
-		$compare = 'width';
-	} else {
-		$width = 0;
-		$compare = 'height';
-	}
-
-// IF IMAGE IS LARGER THAN TARGET, RESIZE
-if($imageprops[$compare] > $$compare) {
-	$image->scaleImage($width, $height);
-	$image->writeImage($path);
+    die(json_encode(array('id' => $id, 'filename' => $name)));
 }
-
-die(json_encode(array('id' => $id, 'filename' => $name)));
+else {
+    die(json_encode(array('id' => '1234', 'filename' => 'error')));
+}
