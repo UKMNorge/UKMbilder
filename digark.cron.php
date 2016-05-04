a5e5bafc2c08e843ff7a90a32a4271cbb4742202
ukmno <ukmno@ukm.no>
2016-05-04 15:57:34 +0200

    Håndte non-UTF8-tegn i UTF8-strenger

--- digark.cron.php
+++ digark.cron.php
@@ -74,8 +74,23 @@ foreach( $files as $file_name ) {
 		$fotograf = $cache_fotograf[ $metadata['wp_uid'] ];
 		
 		$dropbox_name .= ' (PHOTO by UKM Media '. ucfirst($fotograf).')';
-		
+
 		$dropbox_name = preg_replace('/[^\da-z \- æøå]/i', '', $dropbox_name );
+
+$regex = <<<'END'
+/
+  (
+    (?: [\x00-\x7F]                 # single-byte sequences   0xxxxxxx
+    |   [\xC0-\xDF][\x80-\xBF]      # double-byte sequences   110xxxxx 10xxxxxx
+    |   [\xE0-\xEF][\x80-\xBF]{2}   # triple-byte sequences   1110xxxx 10xxxxxx * 2
+    |   [\xF0-\xF7][\x80-\xBF]{3}   # quadruple-byte sequence 11110xxx 10xxxxxx * 3 
+    ){1,100}                        # ...one or more times
+  )
+| .                                 # anything else
+/x
+END;
+$dropbox_name = preg_replace($regex, '$1', $dropbox_name);
+
 		// UPLOAD TO DROPBOX
 		
 		$res = $client->uploadFile('/UKMdigark/Bilder/'. $path . $dropbox_name. strtolower($ext) , Dropbox\WriteMode::add(), $file, $size);
