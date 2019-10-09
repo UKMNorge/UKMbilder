3a35d8cdda754cc14e1d371e78aee3fb24fe014d
Marius Mandal <mariusmandal@gmail.com>
2019-10-08 20:09:50 -0700

    Fjernet noen APIv1-funksjoner

--- ajax_program.inc.php
+++ ajax_program.inc.php
@@ -3,7 +3,6 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-require_once('UKM/forestilling.class.php');
 require_once('UKM/innslag.class.php');
 require_once('UKM/monstring.class.php');
 
